b320b41f43d3f509646e38ab80db81fe06e71fd4
heito <heito@HDR-2023>
2023-06-21 12:42:47 -0300

    Add project files.

---
+++ ChallengeOldPad.Tests/ProgramTest.cs
@@ -0,0 +1,116 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeOldPad.Tests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        [DataRow("0#", " ")]
+        [DataRow("1#", "&")]
+        [DataRow("11#", "'")]
+        [DataRow("111#", "(")]
+        [DataRow("2#", "a")]
+        [DataRow("22#", "b")]
+        [DataRow("222#", "c")]
+        [DataRow("3#", "d")]
+        [DataRow("33#", "e")]
+        [DataRow("333#", "f")]
+        [DataRow("4#", "g")]
+        [DataRow("44#", "h")]
+        [DataRow("444#", "i")]
+        [DataRow("5#", "j")]
+        [DataRow("55#", "k")]
+        [DataRow("555#", "l")]
+        [DataRow("6#", "m")]
+        [DataRow("66#", "n")]
+        [DataRow("666#", "o")]
+        [DataRow("7#", "p")]
+        [DataRow("77#", "q")]
+        [DataRow("777#", "r")]
+        [DataRow("7777#", "s")]
+        [DataRow("8#", "t")]
+        [DataRow("88#", "u")]
+        [DataRow("888#", "v")]
+        [DataRow("9#", "w")]
+        [DataRow("99#", "x")]
+        [DataRow("999#", "y")]
+        [DataRow("9999#", "z")]
+        public void TestOldPhonePad_SendOneCharacter_ReturnCorrectOutput(string input, string expectedOutput)
+        {
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestMethod]
+        [DataRow("1112 222 3 333111#", "(acdf(")]
+        [DataRow("8333527777 222 3 333111#", "tfjascdf(")]
+        [DataRow("4433555 555666#", "hello")]
+        [DataRow("4433555 555666#", "hello")]
+        [DataRow("227*#", "b")]
+        [DataRow("33#", "e")]
+        public void TestOldPhonePad_SendMultipleCharacters_ReturnCorrectOutput(string input, string expectedOutput)
+        {
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestMethod]
+        [DataRow("0wq#")]
+        [DataRow("amareloverdecinza12345#")]
+        [DataRow("123*ik#")]
+        [DataRow("-5#")]
+        public void TestMain_InputWithInvalidCharacter_ThrowException(string input)
+        {
+            string messageExpected = "Invalid Input!";
+            var exception = Assert.ThrowsException<Exception>(() => Program.OldPhonePad(input));
+
+            Assert.AreEqual(messageExpected, exception.Message);
+        }
+        [TestMethod]
+        [DataRow("0wq")]
+        [DataRow("amareloverdecinza12345")]
+        [DataRow("-5")]
+        [DataRow("7777")]
+        [DataRow("8")]
+        [DataRow("88")]
+        [DataRow("1112 222 3 333111")]
+        public void TestMain_InputWithoutSendComand_ThrowException(string input)
+        {
+            string messageExpected = "Without send command!";
+            var exception = Assert.ThrowsException<Exception>(() => Program.OldPhonePad(input));
+
+            Assert.AreEqual(messageExpected, exception.Message);
+        }
+        [TestMethod]
+        [DataRow(" ")]
+        [DataRow("#*#*")]
+        [DataRow("*")]
+        [DataRow("#")]
+        public void TestMain_InputNullOrEmpty_ThrowException(string input)
+        {
+            string messageExpected = "Input null or Empty!";
+            var exception = Assert.ThrowsException<Exception>(() => Program.OldPhonePad(input));
+
+            Assert.AreEqual(messageExpected, exception.Message);
+        }
+
+        class TestClass
+        {
+            static void Main(string[] args)
+            {
+                // Display the number of command line arguments.
+              //  Console.WriteLine(args.Length);
+            }
+        }
+    }
+}
+
---
+++ ChallengeOldPad/Program.cs
@@ -0,0 +1,143 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChallengeOldPad
+{
+    public class Program
+    {
+
+        // Dicionario com os comandos permitidos
+        private static readonly Dictionary<string, string> dicPad = new Dictionary<string, string>()
+             {
+                                                {"*","*"},
+                                                {"#","send"},
+                                                {"0"," "},
+                                                {"1","&"},
+                                                {"11","'"},
+                                                {"111","("},
+                                                {"2", "a"},
+                                                {"22", "b"},
+                                                {"222", "c"},
+                                                {"3", "d"},
+                                                {"33", "e"},
+                                                {"333", "f"},
+                                                {"4", "g"},
+                                                {"44", "h"},
+                                                {"444", "i"},
+                                                {"5","j"},
+                                                {"55","k"},
+                                                {"555","l"},
+                                                {"6","m"},
+                                                {"66","n"},
+                                                {"666","o"},
+                                                {"7","p"},
+                                                {"77","q"},
+                                                {"777","r"},
+                                                {"7777","s"},
+                                                {"8","t"},
+                                                {"88","u"},
+                                                {"888","v"},
+                                                {"9","w"},
+                                                {"99","x"},
+                                                {"999","y"},
+                                                {"9999","z"},
+              };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Retorna o comando enviado pelo usuario
+        /// </summary>
+        /// <param name="input"> Comando enviado pelo usuario, devendo conter somente os caracteres permitidos</param>
+        /// <returns></returns>
+        public static string OldPhonePad(string input)
+        {
+            VerifyInput(input);
+            string output = "";
+            var inputList = ReturnInputList(input);
+
+            foreach (var inputCommand in inputList)
+            {
+                string tempInmputCommand = inputCommand;
+
+                foreach (var key in dicPad.Keys.OrderByDescending(keys => keys.Length).ThenByDescending(keys => keys))
+                {
+                    if (tempInmputCommand.Contains(key))
+                    {
+                        tempInmputCommand = tempInmputCommand.Replace(key, dicPad[key]);
+                    }
+                }
+
+                tempInmputCommand = DeleteCharacter(tempInmputCommand);
+                output += tempInmputCommand;
+            }
+
+
+            return output;
+        }
+
+        /// <summary>
+        /// Apaga os caracteres conforme solicitação do usuário
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private static string DeleteCharacter(string input)
+        {
+            do
+            {
+                int indexToDelete = input.IndexOf('*') - 1;
+
+                if (indexToDelete < 0)
+                    break;
+
+                input = input.Remove(indexToDelete, 1);
+                input = input.Replace("*", "");
+            }
+            while (input.Contains("*"));
+
+            return input;
+        }
+
+        /// <summary>
+        /// Retorna os comandos enviados formatados e separados por espaço
+        /// </summary>
+        /// <param name="input">Comandos enviados pelo usuario</param>
+        /// <returns>Retorno dos comandos enviados em formato de lista de string</returns>
+        private static string[] ReturnInputList(string input)
+        {
+            input = input.Trim();
+            input = input.Remove(input.LastIndexOf('#'));
+
+            return input.Split(" ");
+        }
+
+        /// <summary>
+        /// Valida a entrada fornecida
+        /// </summary>
+        /// <param name="input">Comandos enviados pelo usuario</param>
+        private static void VerifyInput(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input) || (input.Count(a => a != '#' && a != '*') <= 0))
+                throw new Exception("Input null or Empty!");
+
+            if (input.Last() != '#')
+                throw new Exception("Without send command!");
+
+            foreach (var valueChar in input)
+            {
+                if (string.IsNullOrWhiteSpace(valueChar.ToString()) == false && dicPad.Keys.Contains(valueChar.ToString()) == false)
+                    throw new Exception("Invalid Input!");
+
+            }
+
+        }
+    }
+}
+
